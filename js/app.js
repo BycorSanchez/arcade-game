61f3b8c2ba02ba43de39b06416d842ee25777db6
Bycor Sánchez <bycor1989@gmail.com>
2018-06-01 19:25:46 +0100

    add player funcitonality

--- js/app.js
+++ js/app.js
@@ -1,10 +1,7 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+let Enemy = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -16,24 +13,56 @@ Enemy.prototype.update = function(dt) {
     // all computers.
 };
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
+let Player = function(x, y){
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-boy.png';
+}
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+Player.prototype.update = function(dt){
+    // User canvas constrains
+    if (this.x < 0) this.x = 0;
+    if (this.y < 0) this.y = 0;
+    if (this.x > 400) this.x = 400;
+    if (this.y > 380) this.y = 380;
+};
 
+// Draw player on screen
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
+// Control player movement
+Player.prototype.handleInput = function(key){
+    switch(key){
+        case 'left':
+            this.x -= 101;
+            break;
+        case 'right':
+            this.x += 101;
+            break;
+        case 'up':
+            this.y -= 85;
+            break;
+        case 'down':
+            this.y += 85;
+            break;
+    }
+};
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Enemies
+let allEnemies = [new Enemy(), new Enemy(), new Enemy(), new Enemy()];
+
+// Create player at start location
+let player = new Player(200, 380);
+
+// Listen for key presses and and sendit to player's handleInput() method
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
